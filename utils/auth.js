b6bbd239f200f5c52710524a6e4790ca995b989d
Aryan <aryanaggarwal2311@gmail.com>
2025-07-26 07:33:44 +0530

    Added authentication functionality

---
+++ utils/auth.js
@@ -0,0 +1,38 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+export const hashPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+}
+
+export const comparePassword = async (password, hashedPassword) => {
+    return await bcrypt.compare(password, hashedPassword);
+}
+
+export const generateToken = (user) => {
+    return jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1d' });
+}
+
+export const verifyToken = (token) => {
+    try {
+        return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
+        throw new Error('Invalid token');
+    }
+}
+
+export const authenticateUser = async (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
+
+    try {
+        const decoded = verifyToken(token);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(403).json({ message: 'Failed to authenticate token' });
+    }
+}
