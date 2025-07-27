60c0000a864d8169a94a03037fc0d6145f91bf03
Aryan <aryanaggarwal2311@gmail.com>
2025-07-27 19:30:21 +0530

    Added Customer Logic

---
+++ database/repository/Customer_Repository.js
@@ -0,0 +1,101 @@
+import express from 'express';
+import Customer from '../models/Customer_Model.js';
+
+class CustomerRepository {
+    CreateCustomer = async (username, email, password) => {
+    const newCustomer = new Customer({
+        username,
+        email,
+        password});
+    const response = await newCustomer.save();
+        console.log("Customer signed up successfully:", response);
+    return {data: response};
+}
+
+    CreateAddress = async ({ _id, street, postalCode, city, country }) => {
+        const profile = await Customer.findById(_id);
+        if (profile) {
+            const newAddress = new AddressModel({
+                street,
+                postalCode,
+                city,
+                country,
+            });
+
+            await newAddress.save();
+
+            profile.address.push(newAddress._id);
+        }
+        return await profile.save();
+
+    }
+
+    FindCustomerByEmail = async (email) => {
+        const customer = await Customer.findOne({ email});
+        if (!customer) {
+            throw new Error('User not found');
+        }
+        return customer;
+    }
+
+    FindCustomerById = async (id) => { 
+        const customer = await Customer.findById(id
+            .populate('cart.product')
+            .populate('address')
+            .populate('wishlist')
+            .populate('orders')
+        );
+        if (!customer) {
+            throw new Error('User not found');
+        }
+        return customer;
+    }
+
+    async Wishlist(customerid){
+        const customer = await Customer.findById(customerid)
+            .populate('wishlist');
+        return profile.Wishlist;
+    }
+
+    async AddToWishlist(customerId, productId) {
+    await Customer.updateOne(
+        { _id: customerId },
+        { $addToSet: { wishlist: productId } }
+    );
+    return await Customer.findById(customerId).populate('wishlist');
+    }
+    async RemoveFromWishlist(customerId, productId) {
+        await Customer.updateOne(
+            { _id: customerId },
+            { $pull: { wishlist: productId } }
+        );
+        return await Customer.findById(customerId).populate('wishlist');
+    }
+    async updateCartItem(customerId, productId, qty, isRemove) {
+    if (isRemove) {
+        await Customer.updateOne(
+        { _id: customerId },
+        { $pull: { cart: { product: productId } } }
+        );
+    } else {
+        // Try update existing
+        const updateResult = await Customer.updateOne(
+        { _id: customerId, "cart.product": productId },
+        { $set: { "cart.$.unit": qty } }
+        );
+
+        // If not found, push new item
+        if (updateResult.modifiedCount === 0) {
+        await Customer.updateOne(
+            { _id: customerId },
+            { $push: { cart: { product: productId, unit: qty } } }
+        );
+        }
+    }
+
+  return await Customer.findById(customerId).populate("cart.product");
+}
+
+
+}
+export default new CustomerRepository();
---
+++ services/Customer_Service.js
@@ -0,0 +1,70 @@
+import express from 'express';
+import CustomerRepository from '../database/repository/Customer_Repository.js';
+// import Customer from '../database/models/Customer_Model.js';
+import {hashPassword, comparePassword, generateToken} from '../utils/auth.js';
+
+
+export default class CustomerService {
+
+    constructor(){
+        this.repository = CustomerRepository;
+    }
+
+    async signup(username, email, password) {
+
+            const hashedPassword = await hashPassword(password);
+
+
+            const response = await this.repository.CreateCustomer(username, email, hashedPassword);
+            // console.log("Response in servide", response);
+            return response;
+    }
+
+    async login(email, password) {
+        const customer = await CustomerRepository.FindCustomerByEmail({ email });
+        const isMatch = await comparePassword(password, customer.password);
+        if (!isMatch) {
+            throw new Error('Invalid password');
+        }
+        return {
+            token: generateToken(customer),
+            user: {
+                id: customer._id,
+                username: customer.username,
+                email: customer.email
+            }
+        };
+
+    }
+    async AddNewAddress(_id,userInputs){
+    
+    const { street, postalCode, city,country} = userInputs;
+        const addressResult = await this.repository.CreateAddress({ _id, street, postalCode, city,country})
+        return addressResult;
+
+    }
+
+    async ManageCart(customerId, product, qty, isDelete) {
+        
+        const cartResult = await this.repository.AddCartItem(customerId, product, qty, isDelete);
+        return cartResult;     
+    }
+    async GetCustomerDetails(id) {
+        const customer = await this.repository.FindCustomerById(id);
+        if (!customer) {
+            throw new Error('User not found');
+        }
+        return customer;
+    }
+    async GetWishList(customerId){
+            const wishListItems = await this.repository.Wishlist(customerId);
+            return wishListItems;
+        }
+
+    async AddToWishlist(customerId, product){
+            const wishlistResult = await this.repository.AddToWishlist(customerId, product);        
+           return FormateData(wishlistResult);
+    }
+
+
+}
