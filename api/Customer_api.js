99bcd1979bd1ab50446a10a1604fe6fea668e4cd
Aryan <aryanaggarwal2311@gmail.com>
2025-07-27 19:41:10 +0530

    Added other Customer routes

--- api/Customer_api.js
+++ api/Customer_api.js
@@ -28,33 +28,90 @@ router.post('/login', async (req, res, next)=>{
     }
 });
 
-    router.put('/cart',UserAuth, async (req,res,next) => {
-        
-        const { _id, qty } = req.body;
-        
-        try {     
-            const product = await productService.GetProductById(_id);
+router.put('/cart',UserAuth, async (req,res,next) => {
     
-            const result =  await service.ManageCart(req.user._id, product, qty, false);
+    const { _id, qty } = req.body;
     
-            return res.status(200).json(result);
-            
-        } catch (err) {
-            next(err)
-        }
-    });
-    
-    router.delete('/cart/:id',UserAuth, async (req,res,next) => {
+    try {     
+        const product = await productService.getProductById(_id);
 
-        const { _id } = req.user;
+        const result =  await service.ManageCart(req.user._id, product, qty, false);
 
-        try {
-            const product = await productService.getProductById(req.params.id);
-            const result = await service.ManageCart(_id, product, 0 , true);             
-            return res.status(200).json(result);
-        } catch (err) {
-            next(err)
-        }
+        return res.status(200).json(result);
+        
+    } catch (err) {
+        next(err)
+    }
+});
+
+router.delete('/cart/:id',UserAuth, async (req,res,next) => {
+
+    const { _id } = req.user;
+
+    try {
+        const product = await productService.getProductById(req.params.id);
+        const result = await service.ManageCart(_id, product, 0 , true);             
+        return res.status(200).json(result);
+    } catch (err) {
+        next(err)
+    }
+});
+
+router.post("/address", UserAuth, async (req, res, next) => {
+    try {
+    const { _id } = req.user;
+
+    const { street, postalCode, city, country } = req.body;
+
+    const { data } = await service.AddNewAddress(_id, {
+        street,
+        postalCode,
+        city,
+        country,
     });
+
+    return res.json(data);
+    } catch (err) {
+    next(err);
+    }
+});
+
+router.get('/profile', UserAuth, async (req, res, next) => {
+    try {
+        const customer = await service.GetCustomerDetails(req.user._id);
+        return res.status(200).json(customer);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get('/wishlist', UserAuth, async (req, res, next) => {
+    try {
+        const wishListItems = await service.GetWishList(req.user._id);
+        return res.status(200).json(wishListItems);
+    } catch (err) {
+        next(err);
+    }
+});
+router.post('/wishlist', UserAuth, async (req, res, next) => {
+    try {
+        const { productId } = req.body;
+        const wishlistResult = await service.AddToWishlist(req.user._id, productId);
+        return res.status(200).json(wishlistResult);
+    } catch (err) {
+        next(err);
+    }
+});
+router.delete('/wishlist/:productId', UserAuth, async (req, res, next) => {
+    try {
+        const { productId } = req.params;
+        const wishlistResult = await service.RemoveFromWishlist(req.user._id, productId);
+        return res.status(200).json(wishlistResult);
+    } catch (err) {
+        next(err);
+    }
+});
+
+
 
 
