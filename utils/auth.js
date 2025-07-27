069be2b1b88cbe92b4b32c0ff38aabd4352a6c66
Aryan <aryanaggarwal2311@gmail.com>
2025-07-27 22:45:09 +0530

    Improved Authentication

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-
+import Customer from '../database/models/Customer_Model.js';
 export const hashPassword = async (password) => {
     const salt = await bcrypt.genSalt(10);
     return await bcrypt.hash(password, salt);
@@ -14,7 +14,7 @@ export const generateToken = (user) => {
     return jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1d' });
 }
 
-export const verifyToken = (token) => {
+export const verifyToken = async (token) => {
     try {
         return jwt.verify(token, process.env.JWT_SECRET);
     } catch (error) {
@@ -29,8 +29,13 @@ export const authenticateUser = async (req, res, next) => {
     }
 
     try {
-        const decoded = verifyToken(token);
+        const decoded = await verifyToken(token);
         req.user = decoded;
+        const user = await Customer.findById(decoded.id); 
+
+        if (!user) {
+        return res.status(401).json({ message: 'User no longer exists' });
+        }
         console.log("Authenticated user:", req.user);
         next();
     } catch (error) {
